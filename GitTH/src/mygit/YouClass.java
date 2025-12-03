9ac5250df628714e785df1e072bb599d1ecd244e
jeongth22 <jeongth22@skuniv.ac.kr>
2025-12-03 14:31:42 +0900

    Change in master branch

--- GitTH/src/mygit/YouClass.java
+++ GitTH/src/mygit/YouClass.java
@@ -3,5 +3,7 @@ package mygit;
 public class YouClass {
 	public static void main(String toWhom) {
 		System.out.println("Hello" + toWhom);
+		System.out.println("You change in master branch ");
+
 	}
 }
