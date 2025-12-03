0625bc9b33523dfd0efd614219a7d8c5b1da5136
jeongth22 <jeongth22@skuniv.ac.kr>
2025-12-03 14:20:31 +0900

    첫번재 커밋 in dev branch

--- GitTH/src/mygit/Myclass.java
+++ GitTH/src/mygit/Myclass.java
@@ -4,6 +4,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("This is the 2nd Version. ");
-
+		YouClass you = new YouClass();
+		
 	}
 }
