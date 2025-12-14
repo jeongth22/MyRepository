ec7a0caca8b55cd99c163b52097a4972cad6e801
jeongth22 <jeongth22@skuniv.ac.kr>
2025-12-14 22:49:47 +0900

    A B C 커밋

--- GitTH/src/mygit/Myclass.java
+++ GitTH/src/mygit/Myclass.java
@@ -4,7 +4,13 @@ public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("This is the 2nd Version. ");
+<<<<<<< HEAD
 		System.out.println("My changed in master branch ");
 
+=======
+		YouClass you = new YouClass();
+		
+		you.sayHello("Everyone");
+>>>>>>> refs/heads/dev
 	}
 }
